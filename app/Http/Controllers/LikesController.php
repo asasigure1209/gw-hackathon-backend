772b434d4f4d22c4475710f4b22ca54693610147
minami.hitoki <minami.hitoki@a-tm.co.jp>
2020-05-04 17:11:52 +0900

    add: DB Model Controller

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Like;
+
 class LikesController extends Controller
 {
     /**
@@ -13,7 +15,8 @@ class LikesController extends Controller
      */
     public function index()
     {
-        //
+        $like = Like::all();
+        return $like;//
     }
 
     /**
@@ -34,7 +37,18 @@ class LikesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $like = new Like;
+        $like->user_id = $request->user_id;
+        $like->post_id = $request->post_id;
+        $like->save();
+
+        return json_encode(
+            array(
+                "id" => $like->id,
+                "post_id" => $like->name,
+                "post_id" => $like->post_id,
+            )
+        );//
     }
 
     /**
@@ -45,7 +59,8 @@ class LikesController extends Controller
      */
     public function show($id)
     {
-        //
+        $like = Like::find($id);
+        return $like;//
     }
 
     /**
@@ -79,6 +94,13 @@ class LikesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $like = Like::find($id);
+        if($like!=NULL){
+            $like->delete();
+            return response('ステータスコード200', 200);
+        }
+        else{
+            return response('ステータスコード400', 400);
+        }
     }
 }
--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,16 +4,61 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
+use App\Category;
+use App\KipUser;
+use App\Like;
+use App\Useful;
+
+
 class PostsController extends Controller
 {
+    
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+
+
+    public function index(Request $request)
     {
-        //
+        $posts = Post::orderBy($request->type, 'asc')->
+        where('category_id',$request->category)->
+        offset($request->offset)->
+        limit($request->limit)->get();
+        // jsonに変換
+        $cun=0;
+        if(empty($posts[$cun])){
+            return response('ステータスコード400', 400);
+        }
+        else{
+
+            
+            while(!empty($posts[$cun])){
+                $user = KipUser::find($posts[$cun]->user_id);
+                $like = Like::count($posts[$cun]->id);
+                $useful = Useful::count($posts[$cun]->id);
+                $outlines[]=
+                    array(
+                        "id" => $posts[$cun]->id,
+                        "user" => array("id" => $user->id,
+                        "uid" => $user->uid,
+                        "name" => $user->name,
+                        "image" => $user->image_path,
+                        ),
+                        "like_users" => $like,
+                        "useful_count" => $useful,
+                        "category" => $posts[$cun]->category_id,
+                        "content" => $posts[$cun]->content,
+                        "created_at" => $posts[$cun]->created_at,
+                        "update_at" => $posts[$cun]->update_at,
+                    );
+                    $cun++;
+            }
+            return $outlines;
+        }
     }
 
     /**
@@ -34,7 +79,31 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = KipUser::find($request->user_id);
+        $post = new Post;
+        $post->user_id = $request->user_id;
+        $post->content = $request->content;
+        $post->category_id = $request->category_id;
+        $post->save();
+        $like = Like::count($post->id);
+        $useful = Useful::count($post->id);
+        // jsonに変換
+        return json_encode(
+            array(
+                "id" => $post->id,
+                "user" => array("id" => $user->id,
+                "uid" => $user->uid,
+                "name" => $user->name,
+                "image" => $user->image_path,
+                ),
+                "like_users" => $like,
+                "useful_count" => $useful,
+                "category" => $post->category_id,
+                "content" => $post->content,
+                "created_at" => $post->created_at,
+                "update_at" => $post->update_at,
+            )
+        );//
     }
 
     /**
@@ -45,7 +114,32 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        if($post = Post::find($id)==NULL){
+            return response('ステータスコード400', 400);
+        }
+        else{
+        $post = Post::find($id);
+        $user = KipUser::find($post->user_id);
+        $like = Like::count($post->id);
+        $useful = Useful::count($post->id);
+        // jsonに変換
+        return json_encode(
+            array(
+                "id" => $post->id,
+                "user" => array("id" => $user->id,
+                "uid" => $user->uid,
+                "name" => $user->name,
+                "image" => $user->image_path,
+                ),
+                "like_users" => $like,
+                "useful_count" => $useful,
+                "category" => $post->category_id,
+                "content" => $post->content,
+                "created_at" => $post->created_at,
+                "update_at" => $post->update_at,
+            )
+        );//
+    }
     }
 
     /**
@@ -68,7 +162,29 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $post = Post::find($id);
+        $post->user_id = $request->user_id;
+        $post->content = $request->content;
+        $post->category = $request->category;
+        $like = Like::count($post->id);
+        $useful = Useful::count($post->id);
+        // jsonに変換
+        return json_encode(
+            array(
+                "id" => $post->id,
+                "user" => array("id" => $user->id,
+                "uid" => $user->uid,
+                "name" => $user->name,
+                "image" => $user->image_path,
+                ),
+                "like_users" => $like,
+                "useful_count" => $useful,
+                "category" => $post->category_id,
+                "content" => $post->content,
+                "created_at" => $post->created_at,
+                "update_at" => $post->update_at,
+            )
+        );//
     }
 
     /**
@@ -79,6 +195,13 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        if($post!=NULL){
+            $post->delete();
+            return response('ステータスコード200', 200);
+        }
+        else{
+            return response('ステータスコード400', 400);
+        }//
     }
 }
